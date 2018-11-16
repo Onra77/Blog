b2257f9bc54903f1fa28f2e8d55c8c17ca99ec7e
Onra77 <44801402+Onra77@users.noreply.github.com>
2018-11-16 16:48:09 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
     
     <form action="search.php" method="GET">
-        <input type="text" name="query" />
+        <input placeholder="Werkt helaas nog niet. :(" type="text" name="query" />
         <input type="submit" value="Search" />
     </form>
     
